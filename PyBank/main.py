10665ed17af27c76bc03f3534e411c8f8447af0e
HeyMikeMarshall <mjmarshall@gmail.com>
2019-03-30 10:47:58 -0400

    math correction

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,28 +9,37 @@ max_imth = 0
 net = 0
 avg = 0
 term = 0
+i = 1
+d = 0
+dlist = []
 
 with open(budget_data, newline='') as f:
-    reader = csv.reader(f, delimiter=',')
-    header = next(reader)
-    for row in reader:
-        term += 1
-        net += int(row[1])
-        if max_imth < int(row[1]):
-            max_imth = int(row[1])
-            max_smth = row[0]
-        if min_imth > int(row[1]):
-            min_imth = int(row[1])
-            min_smth = row[0]
-avg = round((net / term), 2)
+    reader = csv.reader(f)
+    data = list(reader)
+    term = len(data)
 
+while i < (term - 1): 
+    if i == 0:
+        i += 1
+        net += (int(data[i][1]))
+    else:
+        i += 1
+        d += 1
+        net += (int(data[i][1]))
+        dlist.append(int(data[i][1])-int(data[d][1]))
+avg = round((net / term), 2)
+max_imth = max(dlist)
+max_smth = str(data[dlist.index(max(dlist))+2][0])
+min_imth = min(dlist)
+min_smth = str(data[dlist.index(min(dlist))+2][0])
+davg = (round(sum(dlist)/len(dlist),2))
 
 text = (f"""
 Financial Analysis
 -----------------------------------------------
 Term (in mths): {term}
 Net PL in Term: $ {net}
-Avg PL in Term: $ {avg}
+Avg PL in Term: $ {davg}
 Greatest Increase: {max_smth}  $ {max_imth}
 Greatest Decrease: {min_smth}  $ {min_imth}
 """)
